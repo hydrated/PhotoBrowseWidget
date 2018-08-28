bbe48ba291d03ddc9519a2dd064739ccf1336bff
hydra <hydrawork@gmail.com>
2018-08-28 18:45:47 +0800

    Roughly demo for loading the image

--- app/src/main/java/com/awesome/hydra/photobrowsewidget/provider/MyAppWidgetProvider.java
+++ app/src/main/java/com/awesome/hydra/photobrowsewidget/provider/MyAppWidgetProvider.java
@@ -6,11 +6,14 @@ import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.util.Log;
 import android.widget.RemoteViews;
 
 import com.awesome.hydra.photobrowsewidget.R;
 import com.awesome.hydra.photobrowsewidget.service.WidgetService;
 import com.awesome.hydra.photobrowsewidget.util.GetPictureUtil;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.AppWidgetTarget;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,56 +23,56 @@ import bolts.Task;
 
 public class MyAppWidgetProvider extends AppWidgetProvider {
 
-    private List<String> photoGalleries = new ArrayList<>();
+    public static List<String> photoGalleries = new ArrayList<>();
 
-    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        for (int i = 0; i < appWidgetIds.length; ++i) {
+    public void onUpdate(final Context context, final AppWidgetManager appWidgetManager, final int[] appWidgetIds) {
+        GetPictureUtil.getAllGalleryList(context).continueWith(new Continuation<List<String>, Object>() {
+            @Override
+            public Object then(Task<List<String>> task) {
 
-            // Here we setup the intent which points to the StackViewService which will
-            // provide the views for this collection.
-            Intent intent = new Intent(context, WidgetService.class);
-            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetIds[i]);
-            // When intents are compared, the extras are ignored, so we need to embed the extras
-            // into the data so that the extras will not be ignored.
-            intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
-            RemoteViews rv = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
-            rv.setRemoteAdapter(appWidgetIds[i], R.id.stack_view, intent);
+                if (task.getError() == null && task.getResult() != null) {
+                    photoGalleries = task.getResult();
 
-            // The empty view is displayed when the collection has no items. It should be a sibling
-            // of the collection view.
-            rv.setEmptyView(R.id.stack_view, R.id.empty_view);
+                    for (int i = 0; i < appWidgetIds.length; ++i) {
 
-            // Here we setup the a pending intent template. Individuals items of a collection
-            // cannot setup their own pending intents, instead, the collection as a whole can
-            // setup a pending intent template, and the individual items can set a fillInIntent
-            // to create unique before on an item to item basis.
-            Intent toastIntent = new Intent(context, MyAppWidgetProvider.class);
-            toastIntent.setAction("com.example.android.stackwidget.TOAST_ACTION");
-            toastIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetIds[i]);
-            intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
-            PendingIntent toastPendingIntent = PendingIntent.getBroadcast(context, 0, toastIntent,
-                    PendingIntent.FLAG_UPDATE_CURRENT);
-            rv.setPendingIntentTemplate(R.id.stack_view, toastPendingIntent);
+                        // Here we setup the intent which points to the StackViewService which will
+                        // provide the views for this collection.
+                        Intent intent = new Intent(context, WidgetService.class);
+                        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetIds[i]);
+                        // When intents are compared, the extras are ignored, so we need to embed the extras
+                        // into the data so that the extras will not be ignored.
+                        intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
+                        RemoteViews rv = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+                        rv.setRemoteAdapter(R.id.stack_view, intent);
 
-            appWidgetManager.updateAppWidget(appWidgetIds[i], rv);
-        }
+                        // The empty view is displayed when the collection has no items. It should be a sibling
+                        // of the collection view.
+                        rv.setEmptyView(R.id.stack_view, R.id.empty_view);
+
+                        // Here we setup the a pending intent template. Individuals items of a collection
+                        // cannot setup their own pending intents, instead, the collection as a whole can
+                        // setup a pending intent template, and the individual items can set a fillInIntent
+                        // to create unique before on an item to item basis.
+                        Intent toastIntent = new Intent(context, MyAppWidgetProvider.class);
+                        toastIntent.setAction("com.example.android.stackwidget.TOAST_ACTION");
+                        toastIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetIds[i]);
+                        intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
+                        PendingIntent toastPendingIntent = PendingIntent.getBroadcast(context, 0, toastIntent,
+                                PendingIntent.FLAG_UPDATE_CURRENT);
+                        rv.setPendingIntentTemplate(R.id.stack_view, toastPendingIntent);
+
+                        appWidgetManager.updateAppWidget(appWidgetIds[i], rv);
+                    }
+                }
+                return null;
+            }
+        }, Task.UI_THREAD_EXECUTOR);
+
         super.onUpdate(context, appWidgetManager, appWidgetIds);
     }
 
     @Override
     public void onEnabled(Context context) {
         super.onEnabled(context);
-
-        GetPictureUtil.getAllGalleryList(context).continueWith(new Continuation<List<String>, Object>() {
-            @Override
-            public Object then(Task<List<String>> task) throws Exception {
-
-                if (task.getError() == null && task.getResult() != null) {
-                    photoGalleries = task.getResult();
-
-                }
-                return null;
-            }
-        }, Task.UI_THREAD_EXECUTOR);
     }
 }
--- app/src/main/java/com/awesome/hydra/photobrowsewidget/service/WidgetService.java
+++ app/src/main/java/com/awesome/hydra/photobrowsewidget/service/WidgetService.java
@@ -1,17 +1,30 @@
 package com.awesome.hydra.photobrowsewidget.service;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import android.appwidget.AppWidgetManager;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
 
 import com.awesome.hydra.photobrowsewidget.R;
+import com.awesome.hydra.photobrowsewidget.provider.MyAppWidgetProvider;
+import com.awesome.hydra.photobrowsewidget.util.GetPictureUtil;
 import com.awesome.hydra.photobrowsewidget.view.WidgetItem;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.AppWidgetTarget;
+
+import bolts.Continuation;
+import bolts.Task;
 
 public class WidgetService extends RemoteViewsService {
     @Override
@@ -21,9 +34,7 @@ public class WidgetService extends RemoteViewsService {
 }
 
 class StackRemoteViewsFactory implements RemoteViewsService.RemoteViewsFactory {
-    private static final int mCount = 10;
-    private List<WidgetItem> mWidgetItems = new ArrayList<WidgetItem>();
-    private Context mContext;
+    private final Context mContext;
     private int mAppWidgetId;
 
     public StackRemoteViewsFactory(Context context, Intent intent) {
@@ -33,16 +44,6 @@ class StackRemoteViewsFactory implements RemoteViewsService.RemoteViewsFactory {
     }
 
     public void onCreate() {
-        // In onCreate() you setup any connections / cursors to your data source. Heavy lifting,
-        // for example downloading or creating content etc, should be deferred to onDataSetChanged()
-        // or getViewAt(). Taking more than 20 seconds in this call will result in an ANR.
-        for (int i = 0; i < mCount; i++) {
-            mWidgetItems.add(new WidgetItem(i + "!"));
-        }
-
-        // We sleep for 3 seconds here to show how the empty view appears in the interim.
-        // The empty view is set in the StackWidgetProvider and should be a sibling of the
-        // collection view.
         try {
             Thread.sleep(3000);
         } catch (InterruptedException e) {
@@ -53,47 +54,36 @@ class StackRemoteViewsFactory implements RemoteViewsService.RemoteViewsFactory {
     public void onDestroy() {
         // In onDestroy() you should tear down anything that was setup for your data source,
         // eg. cursors, connections, etc.
-        mWidgetItems.clear();
     }
 
     public int getCount() {
-        return mCount;
+        return MyAppWidgetProvider.photoGalleries.size();
     }
 
     public RemoteViews getViewAt(int position) {
-        // position will always range from 0 to getCount() - 1.
+        Log.d("hydrated", "position" + position);
 
-        // We construct a remote views item based on our widget item xml file, and set the
-        // text based on the position.
         RemoteViews rv = new RemoteViews(mContext.getPackageName(), R.layout.widget_item);
-        rv.setTextViewText(R.id.widget_item, mWidgetItems.get(position).text);
+        Uri uri = Uri.fromFile(new File(MyAppWidgetProvider.photoGalleries.get(position)));
 
-        // Next, we set a fill-intent which will be used to fill-in the pending intent template
-        // which is set on the collection view in StackWidgetProvider.
+        try {
+            Bitmap bitmap = MediaStore.Images.Media.getBitmap(mContext.getContentResolver(), uri);
+            rv.setBitmap(R.id.widget_item, "setImageBitmap", bitmap);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
         Bundle extras = new Bundle();
         extras.putInt("Some String", position);
         Intent fillInIntent = new Intent();
         fillInIntent.putExtras(extras);
         rv.setOnClickFillInIntent(R.id.widget_item, fillInIntent);
 
-        // You can do heaving lifting in here, synchronously. For example, if you need to
-        // process an image, fetch something from the network, etc., it is ok to do it here,
-        // synchronously. A loading view will show up in lieu of the actual contents in the
-        // interim.
-        try {
-            System.out.println("Loading view " + position);
-            Thread.sleep(500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        // Return the remote views object.
         return rv;
     }
 
     public RemoteViews getLoadingView() {
-        // You can create a custom loading view (for instance when getViewAt() is slow.) If you
-        // return null here, you will get the default loading view.
         return null;
     }
 
@@ -110,11 +100,6 @@ class StackRemoteViewsFactory implements RemoteViewsService.RemoteViewsFactory {
     }
 
     public void onDataSetChanged() {
-        // This is triggered when you call AppWidgetManager notifyAppWidgetViewDataChanged
-        // on the collection view corresponding to this factory. You can do heaving lifting in
-        // here, synchronously. For example, if you need to process an image, fetch something
-        // from the network, etc., it is ok to do it here, synchronously. The widget will remain
-        // in its current state while work is being done here, so you don't need to worry about
-        // locking up the widget.
+
     }
 }
